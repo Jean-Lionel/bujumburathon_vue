e6bee43a4e0f38eb915ff4e4ee07f0b8647c25ec
Jean-Lionel <nijeanlionel@gmailcom>
2024-06-22 11:47:37 +0200

    init vue route

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 // import App from './App.vue'
 import '../src/styles/jove.css'
 import FeuillePage from './Feuille-page.vue'
+import router from "router"
 
-createApp(FeuillePage).mount('#app')
+createApp(FeuillePage).
+use(router)
+.mount('#app')
