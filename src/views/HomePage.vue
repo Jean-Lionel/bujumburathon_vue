1a20b038db83e71b7a08141b3f9c040b283b666f
JovinMtl <nsanzumukizath@gmail.com>
2024-06-22 14:02:37 +0200

    made homeContainer and servers class.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,12 +4,23 @@
         <div class="servers">
             <div class="oneServer"></div>
         </div>
+        <div class="serverDetail"></div>
     </div>
 </template>
 
 <script setup>
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.homeContainer{
+    width: 100%;
+    height: 100%;
+    background-color: red;
+}
+.servers{
+    display: flex;
+    width: 80vw;
+    height: 70vh;
+    background-color: blue;
+}
 </style>
