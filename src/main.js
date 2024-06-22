40b8bf345c5f0382d0d8d3efad6ef68a589f7115
Jean-Lionel <nijeanlionel@gmailcom>
2024-06-22 12:31:47 +0200

    install vue router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import '../src/styles/jove.css'
 import FeuillePage from './Feuille-page.vue'
 import router from "router"
+import store from "store"
 
 createApp(FeuillePage).
 use(router)
