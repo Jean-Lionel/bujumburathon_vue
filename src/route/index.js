d2fbc7e639764cfeb26896722d0c0808ac4d4be6
JovinMtl <nsanzumukizath@gmail.com>
2024-06-23 12:35:12 +0200

    chart component routed.

--- src/route/index.js
+++ src/route/index.js
@@ -1,9 +1,16 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import HomePage from "../views/HomePage.vue"
+import chart from '@/views/chart.vue'
 
 const routes = [
-  { path: '/', component: HomePage },
+  { 
+    path: '/', component: HomePage 
+  },
+  {
+    path: '/chart',
+    component: chart
+  },
 ]
 
 const router = createRouter({
